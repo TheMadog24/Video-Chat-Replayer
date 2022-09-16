dd1fbb5b6c4ef314e26c441111af97b44f61ac06
rbluer <665978+rbluer@users.noreply.github.com>
2022-09-16 03:03:59 -0400

    Restructured how the Right-Nav and chat box are positioned and controlled.  Most of it now is naturally flowing within the #topBox instead of being hard coded at a place on the page; the elements are now properly nested. Formatting the video counter to be HH:mm:ss.ddd for better look. The generation of the chat pane is still bad, but now it's terminating going through all of the chat messages as soon as the offset time is outside the current video window.  This should free up some processing when earlier in the video. The actual height of the #chat box is dynamically set using jQuery upon startup.

--- public/js/videoChatPlayer.js
+++ public/js/videoChatPlayer.js
@@ -9,7 +9,16 @@ function localFileVideoPlayer() {
 	var videoNode = document.querySelector('video');
 	var chatNode = document.querySelector('#chat');
 	var chatJson;
-	
+
+    var currentChatPos = -1;
+	var currentChatTime = -1;
+
+    var height = $("#topBox").height();
+    $("#topBox > .header").each( function(index, value) {
+        height -= $(value).height();
+    });
+    $("#chat").height( height );
+    
 
 	var displayMessage = function (message, isError) {
 		var element = document.querySelector('#message');
@@ -50,13 +59,20 @@ function localFileVideoPlayer() {
    var updateChat = function(event) {
 		if(chatJson == null) return;
 
-		let currenTime = videoNode.currentTime;
-		chatNode.innerHTML = currenTime+'<br>';
-		chatJson.comments.forEach(function(comment) { 
+		let currentTime = videoNode.currentTime;
+        $("#videoCounter").text( formatVideoCounter( currentTime ));
+
+		chatNode.innerHTML = "";
+		// chatNode.innerHTML = currenTime+'<br>';
+
+		jQuery.each( chatJson.comments, function( index, comment) { 
+		// chatJson.comments.forEach(function(comment) { 
 			
-			if(currenTime >= comment.content_offset_seconds) {
+			if(currentTime >= comment.content_offset_seconds) {
                 
-                let chatLine = $("<div>").addClass("chatline");
+                let chatLine = $("<div>")
+                            .attr( "id", "comment_" + index )
+                            .addClass("chatline flex");
 
                 let chatTime = renderChatTime( comment );
                 let chatBody = renderChatBody( comment );
@@ -65,6 +81,9 @@ function localFileVideoPlayer() {
                 chatLine.append( chatBody );
 
                 chatLine.appendTo( "#chat" );
+            }
+            else {
+                return false;
             }
 		});
 		chatNode.scrollTop = chatNode.scrollHeight;
@@ -117,6 +136,26 @@ function formatElapsedTime( timeSeconds ) {
     return formatted;
 }
 
+function formatVideoCounter( timeSeconds ) {
+
+    var seconds = timeSeconds; 
+    var hours = Math.floor( seconds / TIME_HOUR );
+    seconds -= (hours * TIME_HOUR);
+    var mins = Math.floor( seconds / TIME_MINUTE );
+    seconds -= (mins * TIME_MINUTE);
+
+    var ms = (seconds - Math.floor( seconds )) * 1000;
+
+    seconds = Math.floor( seconds );
+    ms = Math.floor( ms );
+
+    var formatted = pad(hours, 2) + ":" +
+        pad( mins, 2 ) + ":" +
+        pad( seconds, 2 ) + "." +
+        pad( ms, 3 );
+
+    return formatted;
+}
 function pad( num, size ) {
     var str = "00000" + num.toString();
     return str.substring( str.length - size );
