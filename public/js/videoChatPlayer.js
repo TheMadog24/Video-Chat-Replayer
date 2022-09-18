8de214b466e83bdf9662471551b5637aa64ca52e
rbluer <665978+rbluer@users.noreply.github.com>
2022-09-17 22:56:45 -0400

    Fade out message when starting to play the videos.

--- public/js/videoChatPlayer.js
+++ public/js/videoChatPlayer.js
@@ -164,6 +164,8 @@ function localFileVideoPlayer() {
     var updateChat = function(event) {
 		if(chatJson == null) return;
 
+        $( "#message" ).fadeOut( "slow" );
+        
 		let currentTime = videoNode.currentTime;
  
         updateCountersProgressBar();
