f7c791641996d39d5fedff2b653db0757d160422
Madog24 <Theotherguy1223@gmail.com>
2022-11-23 14:57:25 -0500

    Reformatting "giftsub" message types (incomplete)

--- public/js/videoChatPlayer.js
+++ public/js/videoChatPlayer.js
@@ -1614,7 +1614,7 @@ function rendersubgift(comment) {
   // comment.message.body;
   let message = extractMessageFragments(comment);
   let messageNameRemoved = message[0].outerHTML.replace(comment.commenter.display_name, "");
-  let messageBold = messageNameRemoved.replace("!", "</strong>!").replace("sub to ", "sub to </strong>!");
+  let messageBold = message[0].outerHTML.replace("!", "</b>!").replace("sub to ", "sub to <b>");
 
   let colorHex = comment.message.user_color;
   let styles = { color: colorHex };
@@ -1626,7 +1626,7 @@ function rendersubgift(comment) {
   let messagePrefix = $("<span>").addClass("messagePrefix").text(":");
   
   
-  
+  let subgiftcontainer = $("<div>").addClass("subgiftcontainer")
 
   let chatBody = $("<div>").addClass("chatbody no-time issubgift issubmessage")
         .css('border-left-color', accentColor);
@@ -1635,8 +1635,10 @@ function rendersubgift(comment) {
 		let subgift = $('<svg><path class="subgift" fill-rule="evenodd" d="M16 6h2v6h-1v6H3v-6H2V6h2V4.793c0-2.507 3.03-3.762 4.803-1.99.131.131.249.275.352.429L10 4.5l.845-1.268a2.81 2.81 0 01.352-.429C12.969 1.031 16 2.286 16 4.793V6zM6 4.793V6h2.596L7.49 4.341A.814.814 0 006 4.793zm8 0V6h-2.596l1.106-1.659a.814.814 0 011.49.451zM16 8v2h-5V8h5zm-1 8v-4h-4v4h4zM9 8v2H4V8h5zm0 4H5v4h4v-4z" fill-rule="evenodd"/></svg>');
 		chatBody.append(subgift);
 	}
-	chatBody.append(player);
-	chatBody.append(messageBold);
+	// chatBody.append(player);
+	
+	chatBody.append(subgiftcontainer);
+	subgiftcontainer.append(messageBold);
 	return chatBody;
 
 }
@@ -1781,7 +1783,7 @@ function buildFragmentSubResub( fragment ) {
 
 function buildFragmentSubResubTheyve( fragment ) {
   var msg = fragment.text.trim();
-  msg = msg.replace( "for ", "for <b>").replace( "on a ", "on a <b>" )
+  msg = msg.replace( "for ", "for <b>")
       .replace( " months", " months</b>");
   var result = $("<span>")
       .addClass("subResubTheyve")
