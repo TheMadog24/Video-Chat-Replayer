facdcb873bb0022cd1809c427ec2b9e8fd1b70d1
Madog24 <Theotherguy1223@gmail.com>
2022-11-27 23:22:08 -0500

    Fixed issue first word in message would not go to new line if it did not fit.
    fixed by adding leading space.

--- public/js/videoChatPlayer.js
+++ public/js/videoChatPlayer.js
@@ -2047,7 +2047,7 @@ function extractMessageFragments(comment) {
   var fragments = getExpandedMessageFragments( comment.message.fragments, comment );
 
   jQuery.each(fragments, function (index, fragment) {
-    var altName = fragment.text.trim();
+    var altName = " " + fragment.text.trim();
 	//if the message has "bits_spent", it gets sent for processing
     // if (comment.message["bits_spent"]) {
       // console.log("bits sent");
